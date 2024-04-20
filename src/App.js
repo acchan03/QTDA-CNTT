abdfcccc2764e57d1fc7de612006e7815f9e203c
acchan03 <pupumelu12345@gmail.com>
2024-04-20 14:18:30 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import TicTacToe from './components/TicTacToe/TicTacToe';
+
+
+
+function App() {
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
